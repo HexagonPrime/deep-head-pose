a33638fe844c80b062dd9479b550ff9efb4de1ac
HexagonPrime <8053335677@qq.com>
2021-06-22 19:58:06 +0100

    celeba

--- code/test_hopenet_celeba.py
+++ code/test_hopenet_celeba.py
@@ -90,7 +90,8 @@ if __name__ == '__main__':
     model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
     total = 0
 
-    idx_tensor = [idx for idx in xrange(66)]
+    # idx_tensor = [idx for idx in xrange(66)]
+    idx_tensor = [idx for idx in range(66)]
     idx_tensor = torch.FloatTensor(idx_tensor).cuda(gpu)
 
     # yaw_error = .0
