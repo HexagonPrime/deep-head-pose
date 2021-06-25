e19909557935dcf45ffc46720427256f1aa2c449
HexagonPrime <8053335677@qq.com>
2021-06-25 15:45:21 +0100

    for pi-gan generated

---
+++ code/pigan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH --output=1gpu.out
+#SBATCH --mem=40G
+#SBATCH --gres=gpu:1
+#SBATCH --constrain='titan_xp|geforce_gtx_1080_ti'
+
+source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
+conda activate pi-gan
+python test_hopenet_pigan.py
