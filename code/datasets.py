6348be7ccbe7026ec275e9150f15bad331719338
HexagonPrime <8053335677@qq.com>
2021-06-27 15:38:11 +0100

    for pi-gan generated

--- code/datasets.py
+++ code/datasets.py
@@ -608,7 +608,7 @@ class PiGAN(Dataset):
         # self.transform = transforms.Compose(
         #             [transforms.Resize(320), transforms.CenterCrop(256), transforms.ToTensor(), transforms.Normalize([0.5], [0.5]), transforms.RandomHorizontalFlip(p=0.5), transforms.Resize((256, 256), interpolation=0)])
         self.transform = transforms.Compose(
-                    [transforms.ToTensor()])
+                    [transforms.ToTensor(), transforms.Normalize(mean=[0.50612009, 0.42543493, 0.38282761], std=[0.26589054, 0.24521921, 0.24127836])])
 
     def __len__(self):
         return len(self.data)
